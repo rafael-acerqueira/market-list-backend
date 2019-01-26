3659f649b6a31cae0064068d17ee27f1725804d9
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-26 11:23:26 -0300

    Configure server

---
+++ src/config/server.js
@@ -0,0 +1,10 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+app.listen(port, () => console.log(`BACKEND is running at port ${port}`))
