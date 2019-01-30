eb74bea564a64bb54500c9b85ee65897e18364e6
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-01-30 20:37:48 -0300

    Refactor routes

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,47 +1,35 @@
+const express = require('express')
+
 module.exports = function (app) {
 
+  const router = express.Router()
+  app.use('/api', router)
+  
   var supermarkets = require('../api/controllers/supermarketsController')
-
-  app.route('/supermarkets')
-    .get(supermarkets.list)
-    .post(supermarkets.create)
-
-  app.route('/supermarkets/:id')
-    .get(supermarkets.read)
-    .put(supermarkets.update)
-    .delete(supermarkets.delete)
+  router.get('/supermarkets', supermarkets.list)
+  router.post('/supermarkets', supermarkets.create)
+  router.get('/supermarkets/:id', supermarkets.read)
+  router.put('/supermarkets/:id', supermarkets.update)
+  router.delete('/supermarkets/:id', supermarkets.delete)
 
   var productCategories = require('../api/controllers/productCategoriesController')
-
-  app.route('/product-categories')
-    .get(productCategories.list)
-    .post(productCategories.create)
-
-  app.route('/product-categories/:id')
-    .get(productCategories.read)
-    .put(productCategories.update)
-    .delete(productCategories.delete) 
+  router.get('/product-categories', productCategories.list)
+  router.post('/product-categories', productCategories.create)
+  router.get('/product-categories/:id', productCategories.read)
+  router.put('/product-categories/:id', productCategories.update)
+  router.delete('/product-categories/:id', productCategories.delete)
 
   var products = require('../api/controllers/productsController')
+  router.get('/product-categories/:categoryId/products', products.list)
+  router.post('/product-categories/:categoryId/products', products.create)
+  router.get('/products/:id', products.read)
+  router.put('/products/:id', products.update)
+  router.delete('/products/:id', products.delete)    
 
-  app.route('/product-categories/:categoryId/products')
-    .get(products.list)
-    .post(products.create)
-
-  app.route('/products/:id')
-    .get(products.read)
-    .put(products.update)
-    .delete(products.delete)    
-    
-    
   var shoppingLists = require('../api/controllers/shoppingListsController')
-
-  app.route('/shopping-lists')
-    .get(shoppingLists.list)
-    .post(shoppingLists.create)
-
-  app.route('/shopping-lists/:id')
-    .get(shoppingLists.read)
-    .put(shoppingLists.update)
-    .delete(shoppingLists.delete)     
+  router.get('/shopping-lists', shoppingLists.list)
+  router.post('/shopping-lists', shoppingLists.create)
+  router.get('/shopping-lists/:id', shoppingLists.read)
+  router.put('/shopping-lists/:id', shoppingLists.update)
+  router.delete('/shopping-lists/:id', shoppingLists.delete)    
 }
