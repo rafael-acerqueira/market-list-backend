160c43b66fca504d66e91eeaa0b1dddf81c54b97
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-02-18 20:44:08 -0300

    Add created_at and updated_at to supermarkets

--- src/api/models/supermarketModel.js
+++ src/api/models/supermarketModel.js
@@ -3,6 +3,8 @@ var Schema = mongoose.Schema
 
 var SupermarketSchema = new Schema({
   name: { type: String, required: true, trim: true }
-})
+},
+  { timestamps: { createdAt: 'createdDate',updatedAt: 'updatedDate' }}
+)
 
 module.exports = mongoose.model('Supermarket', SupermarketSchema)
