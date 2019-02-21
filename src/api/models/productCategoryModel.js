a33ca3d91124c68f9aa8f1db58c17c8af48fa360
Rafael Aquino <rafael.acerqueirah@gmail.com>
2019-02-20 21:25:11 -0300

    Add created_at and updated_at to product category

--- src/api/models/productCategoryModel.js
+++ src/api/models/productCategoryModel.js
@@ -3,6 +3,8 @@ var Schema = mongoose.Schema
 
 var ProductCategory = new Schema({
   name: { type: String, required: true, trim: true}
-})
+},
+  { timestamps: { createdAt: 'createdDate',updatedAt: 'updatedDate' }}
+)
 
 module.exports = mongoose.model('ProductCategory', ProductCategory)
